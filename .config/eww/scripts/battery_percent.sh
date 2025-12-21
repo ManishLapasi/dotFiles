cc4f3466ec9a89c83b4d2e133d090d7e1cfe7860
manishlapasi <manish.lapasi@gmail.com>
2025-12-21 01:50:05 -0500

    eww + hypr minor mods

--- .config/eww/scripts/battery_percent.sh
+++ .config/eww/scripts/battery_percent.sh
@@ -3,7 +3,7 @@
 battery_status=$(cat /sys/class/power_supply/BAT0/status)
 battery_percent=$(cat /sys/class/power_supply/BAT0/capacity)
 if [[ $battery_status -eq "Charging" ]]; then
-    echo "󰂄 $battery_percent"
+    echo "󰂄 ${battery_percent}%"
 else
-    echo "󰂀 $battery_percent"
+    echo "󰂀 ${battery_percent}%"
 fi
